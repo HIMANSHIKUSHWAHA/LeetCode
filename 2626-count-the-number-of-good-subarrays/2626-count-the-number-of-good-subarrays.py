fe88c3ca7ae82fb0274bd17521255872c4d7f19d
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-04-16 12:18:42 -0700

    Time: 114 ms (85.11%), Space: 34.3 MB (82.85%) - LeetHub

--- 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
@@ -1,16 +1,20 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
         n=len(nums)
+        i=0
         freq=defaultdict(int)
-        ans, cnt, l=0, 0, 0
-        for r,x in enumerate(nums):
-            cnt+=freq[x]
-            freq[x]+=1
-            while cnt>=k:
-                ans+=n-r
-                freq[nums[l]]-=1
-                cnt-=freq[nums[l]]
-                l+=1
-        return ans
+        res=0
+        count=0
+        for j in range(n):
+            num=nums[j]
+            count+=freq[num]
+            freq[num]+=1
+            while count>=k:
+                res+=n-j
+                freq[nums[i]]-=1
+                count-=freq[nums[i]]
+                i+=1
+        return res
 
+        
                
