7d0e9ee8d78ffef0309d4332b5d0dd8aa14e4a20
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-04-16 21:12:38 -0700

    Time: 19 ms (72%), Space: 18 MB (12.5%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,19 +1,19 @@
-from collections import defaultdict
-
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
-        pos_map = defaultdict(list)
-        
-        # Store positions of each number
-        for idx, val in enumerate(nums):
-            pos_map[val].append(idx)
-        
-        count = 0
-        
-        for indices in pos_map.values():
-            for i in range(len(indices)):
-                for j in range(i + 1, len(indices)):
-                    if (indices[i] * indices[j]) % k == 0:
-                        count += 1
-                        
+        s = set(nums)
+        if s==nums:
+            return 0
+        i, count=0,0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] == nums[j]:
+                    product = 0
+                    product = i*j
+                    if product % k == 0:
+                        count+=1
         return count
+                
+
+                
+
+        
