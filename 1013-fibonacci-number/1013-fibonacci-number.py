cd9a875692e91233017bd8e56643feb2198cefd7
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-04-20 13:45:22 -0700

    Time: 48 ms (35.3%), Space: 17.7 MB (41.58%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -1,12 +1,23 @@
 class Solution:
     def fib(self, n: int) -> int:
-        memo={0:0, 1:1}
-        def f(x):
-            if x in memo:
-                return memo[x]
-            else:
-                memo[x]=f(x-1)+f(x-2)
-                memo[x]
-                return memo[x]
-        return f(n)
+        #TOP-DOWN APPROACH
+        # memo={0:0, 1:1}
+        # def f(x):
+        #     if x in memo:
+        #         return memo[x]
+        #     else:
+        #         memo[x]=f(x-1)+f(x-2)
+        #         memo[x]
+        #         return memo[x]
+        # return f(n)
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        d=[0]*(n+1)
+        d[0]=0
+        d[1]=1
+        for i in range(2, n+1):
+            d[i]=d[i-1]+d[i-2]
+        return d[n]
         
