b1a5d07e9c0603b7bc5a34773aa24a46dbf509e7
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-04-20 13:49:07 -0700

    Time: 42 ms (49.1%), Space: 17.5 MB (89.66%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.py
+++ 1013-fibonacci-number/1013-fibonacci-number.py
@@ -10,14 +10,21 @@ class Solution:
         #         memo[x]
         #         return memo[x]
         # return f(n)
+
+        #bottom up approach
         if n==0:
             return 0
         if n==1:
             return 1
-        d=[0]*(n+1)
-        d[0]=0
-        d[1]=1
-        for i in range(2, n+1):
-            d[i]=d[i-1]+d[i-2]
-        return d[n]
+        # d=[0]*(n+1)
+        # d[0]=0
+        # d[1]=1
+        # for i in range(2, n+1):
+        #     d[i]=d[i-1]+d[i-2]
+        # return d[n]
         
+        prev=0
+        curr=1
+        for i in range(2, n+1):
+            prev, curr = curr, prev+curr
+        return curr
