05a66e4582d483f8fcb7a1c4ff4bde54a9a3600b
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-04-20 00:30:30 -0700

    Time: 3 ms (98.97%), Space: 20.9 MB (43.08%) - LeetHub

--- 1039-find-the-town-judge/1039-find-the-town-judge.py
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -1,16 +1,16 @@
+from collections import defaultdict
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        l = len(trust)
-
-        trusts = [0 for i in range(n + 1)]
-        is_trusted = [0 for i in range(n + 1)]
-
-        for i in range(l):
-            trusts[trust[i][0]] += 1
-            is_trusted[trust[i][1]] += 1
-        
-        for i in range(1, n + 1):
-            if trusts[i] == 0:
-                if is_trusted[i] == n - 1:
-                    return i
+        d = defaultdict(list)
+        trusted=[0] * (n + 1)
+        for u, v in trust:
+            d[u].append(v)
+            trusted[v]+=1
+        for i in range(1, n+1):
+            if i not in d and trusted[i]==n-1:
+                return i
         return -1
+        
+
+
+        
