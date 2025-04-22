057455893e764d7ee3647a8a9fa9a55425042f03
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-04-22 13:10:30 -0700

    Time: 0 ms (100%), Space: 21.9 MB (50.77%) - LeetHub

--- 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.py
+++ 2415-count-the-number-of-ideal-arrays/2415-count-the-number-of-ideal-arrays.py
@@ -1,25 +1,47 @@
-class Solution(object):
-    def idealArrays(self, n, maxi):
-        mod = 10**9 + 7
-        m = min(n, 14)
-        dp = [[0] * (m+1) for _ in range(maxi+1)]
-        for i in range(1, maxi+1):
-            dp[i][1] = 1
-            j = 2
-            while i * j <= maxi:
-                for k in range(1, m):
-                    dp[i*j][k+1] += dp[i][k]
-                j += 1
-        fact = [1] * n
-        for i in range(1, n):
-            fact[i] = fact[i-1] * i % mod
-        invfact = [1] * n
-        invfact[n-1] = pow(fact[n-1], mod-2, mod)
-        for i in range(n-1, 0, -1):
-            invfact[i-1] = invfact[i] * i % mod
-        res = 0
-        f_n1 = fact[n-1]
-        for i in range(1, maxi+1):
-            for k in range(1, m+1):
-                res = (res + dp[i][k] * f_n1 % mod * invfact[k-1] % mod * invfact[n-k] % mod) % mod
-        return res
+MODULO = int(1e9+7)
+MAX_VALUE = int(1e4)
+STRICT_COUNTS = [[i+1 for i in range(MAX_VALUE)]]
+prev_row = [1] * MAX_VALUE
+next_row = [0] * MAX_VALUE
+prev_base = 1
+while (prev_base << 1) <= MAX_VALUE:
+    next_base = prev_base << 1
+    for i in range(next_base-1, MAX_VALUE):
+        next_row[i] = 0
+    for prev_num in range(prev_base, MAX_VALUE+1):
+        prev_count = prev_row[prev_num-1]
+        for mult in range(2, MAX_VALUE+1):
+            product = prev_num * mult
+            if product > MAX_VALUE:
+                break
+            out_i = product - 1
+            next_row[out_i] = (next_row[out_i] + prev_count) % MODULO
+    current_counts = [next_row[next_base-1]] * (MAX_VALUE+1-next_base)
+    for next_num in range(next_base+1, MAX_VALUE+1):
+        current_counts[next_num-next_base] = current_counts[next_num-1-next_base] + next_row[next_num-1]
+    STRICT_COUNTS.append(current_counts)
+    prev_base = next_base
+    prev_row, next_row = next_row, prev_row
+    
+#print("Maximum growing length is %d" % len(STRICT_COUNTS))
+
+class Solution:
+    def idealArrays(self, n: int, maxValue: int) -> int:
+        count = 0
+        combo = 1
+        top_factor = n - 1
+        bottom_factor = 1
+        base = 1
+        for k in range(min(n, len(STRICT_COUNTS))):
+            if base <= maxValue:
+                #print(k, combo)
+                #print(STRICT_COUNTS[k][maxValue-base])
+                count = (count + combo * STRICT_COUNTS[k][maxValue-base]) % MODULO
+            else:
+                break
+            combo *= top_factor
+            combo //= bottom_factor
+            top_factor -= 1
+            bottom_factor += 1
+            base <<= 1
+        return count
