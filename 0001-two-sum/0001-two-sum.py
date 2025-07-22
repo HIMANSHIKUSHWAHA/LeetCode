e030590c88416eef6e5a13ae92e8f2901de2abf6
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-07-21 19:30:32 -0700

    Time: 0 ms (100%), Space: 18.8 MB (61.73%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,21 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        n=len(nums)
-        for i in range(n):
-            for j in range(i+1, n):
-                if nums[i] + nums[j] == target:
-                    return i,j
-                
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen, complement = {}, 0
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in seen:
+                return [seen[complement], i]
+            seen[nums[i]]=i
+        
+
+        
+
+
+
+
+
+
+
+        
+            
         
