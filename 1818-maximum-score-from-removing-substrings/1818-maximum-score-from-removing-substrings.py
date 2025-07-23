ec63ca595139e4105d51c32620f2a98d0a6d0e22
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-07-22 20:47:48 -0700

    Time: 139 ms (98.06%), Space: 18.1 MB (96.13%) - LeetHub

--- 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
+++ 1818-maximum-score-from-removing-substrings/1818-maximum-score-from-removing-substrings.py
@@ -1,22 +1,23 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        def remove_pair(s, first, second, score):
-            stack = []
-            total = 0
-            for char in s:
-                if stack and stack[-1] == first and char == second:
-                    stack.pop()
-                    total += score
-                else:
-                    stack.append(char)
-            return "".join(stack), total
-
-        # Priority removal: whichever gives more points first
         if x > y:
-            s, gain1 = remove_pair(s, 'a', 'b', x)
-            _, gain2 = remove_pair(s, 'b', 'a', y)
+            return self.getMax(s, 'a', 'b', x, y)
         else:
-            s, gain1 = remove_pair(s, 'b', 'a', y)
-            _, gain2 = remove_pair(s, 'a', 'b', x)
-
-        return gain1 + gain2
+            return self.getMax(s, 'b', 'a', y, x)
+        
+    def getMax(self, s: str, a:str, b:str, x: int, y: int):
+        c1 = c2 = ans = 0
+        s += 'c'
+        for c in s:
+            if c == a:
+                c1 += 1
+            elif c == b:
+                if c1 == 0:
+                    c2 += 1
+                else:
+                    ans += x
+                    c1 -= 1
+            else:
+                ans += y * min(c1, c2)
+                c1 = c2 = 0
+        return ans
