2c47d8dc60b5062bf600485666cd3543540dac3d
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-07-29 15:24:49 -0700

    Time: 1155 ms (16.48%), Space: 311.4 MB (5.46%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -0,0 +1,14 @@
+from functools import lru_cache
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        if text1==text2:
+            return len(text1)
+        @lru_cache(maxsize=None)
+        def longcomsub(i, j):
+            if i==len(text1) or j==len(text2):
+                return 0
+            if text1[i]==text2[j]:
+                return 1+longcomsub(i+1, j+1)
+            return max(longcomsub(i+1, j), longcomsub(i, j+1))
+        return longcomsub(0,0)
+        
