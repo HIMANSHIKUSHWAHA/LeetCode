00096b6303da73836a44bfbfb466b91aba08f1f5
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-07-31 08:42:31 -0700

    Time: 577 ms (59.25%), Space: 20.6 MB (69.34%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,26 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        n=len(nums)
-        nums=sorted(nums)
-        answer=[]
-        for i in range(n):
-            if nums[i]>0:
-                break
-            elif i>0 and nums[i]==nums[i-1]:
-                continue
-            lo, hi = i+1, n-1
-            while lo<hi:
-                summ=nums[i]+nums[lo]+nums[hi]
-                if summ==0:
-                    answer.append([nums[i], nums[lo], nums[hi]])
-                    lo, hi = lo+1, hi-1
-                    while lo<hi and nums[lo]==nums[lo-1]:
-                        lo+=1
-                    while lo<hi and nums[hi]==nums[hi+1]:
-                        hi-=1
-                elif summ>0:
-                    hi-=1
-                else:
-                    lo+=1
-        return answer
+        nums.sort()
+        res = []
+        
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue  # Skip duplicate first element
+            
+            left, right = i + 1, len(nums) - 1
+            
+            while left < right:
+                s = nums[i] + nums[left] + nums[right]
                 
+                if s < 0:
+                    left += 1
+                elif s > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+                    
+                    # Skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                        
+                    left += 1
+                    right -= 1
+        
+        return res
