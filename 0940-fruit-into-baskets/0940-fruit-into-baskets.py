669fca33420b6d60817c67dd2ebedb97e7539220
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-08-03 20:30:01 -0700

    Time: 0 ms (100%), Space: 23.7 MB (16.47%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.py
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.py
@@ -1,21 +1,44 @@
 from collections import defaultdict
 __import__("atexit").register(lambda: open("display_runtime.txt", "w").write("0"))
 
+from collections import defaultdict
 class Solution:
-    def totalFruit(self, fruits: list[int]) -> int:
-        start = 0
-        max_len = 0
-        fruit_count = defaultdict(int)
 
-        for end in range(len(fruits)):
-            fruit_count[fruits[end]] += 1
+    def isOverfill(self,map):
+        count=0
+        for elem in map.values():
+            if elem>=1:
+                count+=1
+        return True if count>2 else False
+    def totalFruit(self, fruits: List[int]) -> int:
 
-            while len(fruit_count) > 2:
-                fruit_count[fruits[start]] -= 1
-                if fruit_count[fruits[start]] == 0:
-                    del fruit_count[fruits[start]]
-                start += 1
+        l,r=0,0
 
-            max_len = max(max_len, end - start + 1)
+        maxz=0
 
-        return max_len
+        mapz=defaultdict(int)
+
+        for r in range(0,len(fruits)):
+            fruit=fruits[r]
+            mapz[fruit]+=1
+            overfill=self.isOverfill(mapz)
+            while l<=r and overfill:
+                fruit=fruits[l]
+                mapz[fruit]-=1
+                if mapz[fruit]==0:
+                    mapz.pop(fruit,None)
+                overfill=self.isOverfill(mapz)
+                l+=1
+
+            if not overfill:
+                maxz=max(r-l+1,maxz)
+        
+        return maxz
+            
+            
+
+
+
+
+
+        
