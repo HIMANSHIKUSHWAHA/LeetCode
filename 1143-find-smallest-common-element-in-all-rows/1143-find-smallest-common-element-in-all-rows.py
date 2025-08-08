082de5ce3a683062ab68317c4b9347124e5a4562
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-08-08 09:21:03 -0700

    Time: 7 ms (77.1%), Space: 39.8 MB (20.61%) - LeetHub

--- 1143-find-smallest-common-element-in-all-rows/1143-find-smallest-common-element-in-all-rows.py
+++ 1143-find-smallest-common-element-in-all-rows/1143-find-smallest-common-element-in-all-rows.py
@@ -1,14 +1,5 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
-        '''
-            Given an m x n matrix mat where every row is sorted in strictly increasing order,
-            return the smallest common element in all rows
-        '''
-
-        '''
-            Iterate through the first row, use binary search in each other row to try to find that element
-        '''
-
         for num in mat[0]:
             all_contain = True
             for row in mat[1:]:
@@ -17,8 +8,6 @@ class Solution:
                 if index >= len(row) or row[index] != num:
                     all_contain = False
                     break
-
             if all_contain:
                 return num
-
         return -1
