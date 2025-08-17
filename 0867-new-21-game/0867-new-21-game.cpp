7192a6f21fb9d3ffc2f8a039b6efe3aad63302f0
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-08-16 17:01:37 -0700

    Time: 4 ms (26.82%), Space: 12.5 MB (70.12%) - LeetHub

--- 0867-new-21-game/0867-new-21-game.cpp
+++ 0867-new-21-game/0867-new-21-game.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    double new21Game(int N, int K, int maxPts) {
-        // Corner cases
-        if (K == 0) return 1.0;
-        if (N >= K-1 + maxPts) return 1.0;
+    double new21Game(int n, int k, int maxPts) {
+        if (k == 0) return 1.0;
+        if (n >= k + maxPts - 1) return 1.0;
 
-        // dp[i] is the probability of getting point i.
-        vector<double> dp(N + 1, 0.0);
-
-        double probability = 0.0; // dp of N or less points.
-        double windowSum = 1.0; // Sliding required window sum
+        vector<double> dp(n + 1, 0.0);
         dp[0] = 1.0;
-        for (int i = 1; i <= N; i++) {
+        double windowSum = 1.0;
+        double result = 0.0;
+
+        for (int i = 1; i <= n; ++i) {
             dp[i] = windowSum / maxPts;
 
-            if(i < K) windowSum += dp[i];
-            else probability += dp[i];
-            
-            if(i >= maxPts) windowSum -= dp[i - maxPts];
+            if (i < k) {
+                windowSum += dp[i];
+            } else {
+                result += dp[i];
+            }
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
+            }
         }
 
-        return probability;
+        return result;
     }
 };
