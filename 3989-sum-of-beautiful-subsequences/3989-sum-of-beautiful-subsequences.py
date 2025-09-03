1a1a980eed735b7bfcf92d25dfa285e4b26d3783
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-09-02 21:00:15 -0700

    Time: 2836 ms (100%), Space: 48.4 MB (37.72%) - LeetHub

--- 3989-sum-of-beautiful-subsequences/3989-sum-of-beautiful-subsequences.py
+++ 3989-sum-of-beautiful-subsequences/3989-sum-of-beautiful-subsequences.py
@@ -1,47 +1,63 @@
+from collections import defaultdict
+
+M_val = 70000
+
 class Solution:
-    def totalBeauty(self, A: List[int]) -> int:
-        MOD = 10**9 + 7
-        M = max(A) + 1
-        locs = defaultdict(list)
-        for i, x in enumerate(A):
-            locs[x].append(i)
+    precomputed = False
+    phi_g = None
+    divisors_g = None
 
-        F = [0] * M
-        for d in range(1, M):
-            indices = sorted(i for v in range(d, M, d) for i in locs[v])
-            if len(indices) <= 1:
-                F[d] = len(indices)
+    def totalBeauty(self, nums: List[int]) -> int:
+        mod = 10**9 + 7
+        M = M_val
+        if not Solution.precomputed:
+            phi = list(range(M + 1))
+            for i in range(2, M + 1):
+                if phi[i] == i:
+                    for j in range(i, M + 1, i):
+                        phi[j] -= phi[j] // i
+            Solution.phi_g = phi
+
+            divisors = [[] for _ in range(M + 1)]
+            for i in range(1, M + 1):
+                for j in range(i, M + 1, i):
+                    divisors[j].append(i)
+            Solution.divisors_g = divisors
+            Solution.precomputed = True
+
+        phi_arr = Solution.phi_g
+        divisors_arr = Solution.divisors_g
+
+        arr_by_d = defaultdict(list)
+        divisors_appear = set()
+        for x in nums:
+            for d in divisors_arr[x]:
+                if d <= M:
+                    arr_by_d[d].append(x)
+                    divisors_appear.add(d)
+        
+        ans = 0
+        for d in divisors_appear:
+            L = arr_by_d[d]
+            if not L:
                 continue
-
-            rank = {pos: r for r, pos in enumerate(indices, 1)}
-            fen = Fenwick(len(indices))
-            for v in range(d, M, d):
-                for pos in reversed(locs[v]):
-                    r = rank[pos]
-                    addend = 1 + fen.sum(r - 1)
-                    F[d] += addend
-                    fen.add(r, addend)
-
-        for d in range(M - 1, 0, -1):
-            for e in range(2 * d, M, d):
-                F[d] -= F[e]
-            F[d] %= MOD
-
-        return sum(d * F[d] for d in range(1, M)) % MOD
-
-
-class Fenwick:
-    def __init__(self, n):
-        self.a = [0] * (n + 1)
-
-    def sum(self, i):
-        s = 0
-        while i > 0:
-            s += self.a[i]
-            i -= i & -i
-        return s
-
-    def add(self, i, x):
-        while i < len(self.a):
-            self.a[i] += x
-            i += i & -i
+            distinct_vals = sorted(set(L))
+            comp = {val: idx + 1 for idx, val in enumerate(distinct_vals)}
+            n_comp = len(distinct_vals)
+            fenw = [0] * (n_comp + 1)
+            total_count = 0
+            for x in L:
+                pos = comp[x]
+                s = 0
+                idx = pos - 1
+                while idx > 0:
+                    s = (s + fenw[idx]) % mod
+                    idx -= idx & -idx
+                count_here = (1 + s) % mod
+                total_count = (total_count + count_here) % mod
+                idx = pos
+                while idx <= n_comp:
+                    fenw[idx] = (fenw[idx] + count_here) % mod
+                    idx += idx & -idx
+            ans = (ans + phi_arr[d] * total_count) % mod
+        return ans % mod
