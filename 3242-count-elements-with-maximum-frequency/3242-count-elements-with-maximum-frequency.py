8a2f9e0c8f30a56a2f8e64e0b7ea7f57c00fac9f
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-09-22 20:31:59 -0700

    Time: 0 ms (100%), Space: 17.7 MB (86.73%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.py
@@ -1,5 +1,14 @@
 class Solution:
-    def maxFrequencyElements(self, nums: List[int]) -> int:
-        freq = Counter(nums)
-        maxf = max(freq.values())
-        return sum(v for v in freq.values() if v == maxf)
+    def maxFrequencyElements(self, nums: list[int]) -> int:
+        freq = [0] * 101
+        mx = 0
+        res = 0
+        for n in nums:
+            freq[n] += 1
+            f = freq[n]
+            if f > mx:
+                mx = f
+                res = f
+            elif f == mx:
+                res += f
+        return res
