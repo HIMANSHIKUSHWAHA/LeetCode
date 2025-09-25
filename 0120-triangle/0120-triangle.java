804f09c80eccf27cd7ee704124853dd78eb43111
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-09-24 21:45:47 -0700

    Time: 0 ms (100%), Space: 45.7 MB (20.7%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,8 +1,21 @@
-public class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        for(int i = triangle.size() - 2; i >= 0; i--)
-            for(int j = 0; j <= i; j++)
-                triangle.get(i).set(j, triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1)));
-        return triangle.get(0).get(0);
+class Solution {
+    int m;
+    Integer[][] dp;
+    public int minimumTotal(List<List<Integer>> tran) {
+        m = tran.size();
+        dp = new Integer[m][m];
+       
+        return helper(tran,0,0);
+    }
+    int helper(List<List<Integer>> tran,int row, int col){
+        if(row==m){
+            return 0;
+        }
+        if(dp[row][col]!=null){
+            return dp[row][col];
+        }
+        int left = helper(tran,row+1,col);
+        int right = helper(tran,row+1,col+1);
+        return dp[row][col]=tran.get(row).get(col) + Math.min(left,right);
     }
 }
