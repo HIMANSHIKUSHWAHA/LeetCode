d3468b9dabc69b8af4c7dc4dcba7f0fac7ef155c
HIMANSHI KUSHWAHA <88763492+HIMANSHIKUSHWAHA@users.noreply.github.com>
2025-10-26 23:26:36 -0700

    Time: 1 ms (95.91%), Space: 19.5 MB (44.64%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def numberOfBeams(self, bank):
-        prev_row_count = 0
-        total = 0
-
-        for row in bank:
-            cur_row_count = self.calc(row)
-            if cur_row_count == 0:
-                continue
-
-            total += cur_row_count * prev_row_count
-            prev_row_count = cur_row_count
-
-        return total
-
-    def calc(self, s):
-        return sum(int(c) for c in s)
+class Solution:
+    def numberOfBeams(self, bank: List[str]) -> int:
         
+        curr=bank[0].count("1")
+        beams=0
+        for row in bank[1:]:
+            devices=row.count("1")
+            if not devices:
+                continue
+            beams+=devices*curr
+            curr=devices
+        return beams
